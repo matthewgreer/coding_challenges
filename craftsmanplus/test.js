d55cc3a53ceaef9b26ff101d3a534f7ed5092aa6
matthewgreer <matthew.greer.dev@gmail.com>
2021-02-26 11:36:27 -0500

    Created challenge files

---
+++ craftsmanplus/test.js
@@ -0,0 +1,47 @@
+//==================//
+// 		Part 1	 	//
+//==================//
+
+var userBalance = 0;
+
+// Gets called whenever the money finished tweening to the bottom.
+function addFromCatch(value)
+{
+}
+
+// Gets called every frame. 
+// Time elapsed since the last update is passed into the function(milliseconds)
+function onUpdate({delta})
+{
+}
+
+// You have access to a function updateBalance which 
+// takes in a string and sets the ui to that value
+// updateBalance("1");
+
+
+
+
+//==================//
+// 		Part 2	 	//
+//==================//
+
+function processSlots(input)
+{
+	console.log("Output: ");
+};
+
+// examples input
+var array = [
+	[1,0,0,0,1],
+	[0,1,0,1,0],
+	[0,0,1,0,0]
+];
+//Output: '1 winning line, scoring 20 points.'
+
+var array2 = [
+	[2,4,2,2,3],
+	[1,1,1,4,1],
+	[3,3,3,4,2]
+];
+//Output: '2 winning lines, scoring a total 30 points.'
